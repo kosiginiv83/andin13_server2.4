e678cc981749ae52062d3d8a921f8e523d3accfb
kosiginiv83 <kosiginiv83@gmail.com>
2021-02-14 17:17:02 +0800

    Updated

---
+++ src/main/kotlin/ru/netology/nmedia/repository/PostRepository.kt
@@ -0,0 +1,7 @@
+package ru.netology.nmedia.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import ru.netology.nmedia.entity.PostEntity
+
+
+interface PostRepository : JpaRepository<PostEntity, Long>
